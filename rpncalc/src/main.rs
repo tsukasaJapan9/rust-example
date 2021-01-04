2181f5b4cca961e2df5b9eb3ccd042823546dcf5
Tsukasa Horinouchi <tsukasa.horinouchi@groove-x.com>
2021-01-04 22:34:33 +0900

    handle args

--- rpncalc/src/main.rs
+++ rpncalc/src/main.rs
@@ -1,6 +1,25 @@
-use std::env;
+use clap::Clap;
+
+#[derive(Clap, Debug)]
+#[clap(
+    name = "My program",
+    version = "1.0.0",
+    author = "horino",
+    about = "awesome project"
+)]
+struct Opts {
+    #[clap(short, long)]
+    verbose: bool,
+
+    #[clap(name = "FILE")]
+    formula_file: Option<String>,
+}
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    let opts = Opts::parse();
+    match opts.formula_file {
+        Some(file) => println!("File specified: {}", file),
+        None => println!("No file specified"),
+    }
+    println!("verbose status: {}", opts.verbose);
 }
