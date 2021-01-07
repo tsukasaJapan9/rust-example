c4b72505a784449c7a3fb0af9568b0282412d0ad
Tsukasa Horinouchi <tsukasa.horinouchi@groove-x.com>
2021-01-07 21:21:18 +0900

    implement db access

--- todo/src/main.rs
+++ todo/src/main.rs
@@ -1,5 +1,8 @@
-use actix_web::{get, App, HttpResponse, HttpServer, ResponseError};
+use actix_web::{get, web, App, HttpResponse, HttpServer, ResponseError};
 use askama::Template;
+use r2d2::Pool;
+use r2d2_sqlite::SqliteConnectionManager;
+use rusqlite::params;
 use thiserror::Error;
 
 struct TodoEntry {
@@ -16,13 +19,30 @@ struct IndexTemplate {
 enum MyError {
     #[error("Failed to render HTML")]
     AskamaError(#[from] askama::Error),
+
+    #[error("Failed to get connection")]
+    ConnectionPoolError(#[from] r2d2::Error),
+
+    #[error("Failed SQL execution")]
+    SQLiteError(#[from] rusqlite::Error),
 }
 
 impl ResponseError for MyError {}
 
 #[get("/")]
-async fn index() -> Result<HttpResponse, MyError> {
+async fn index(db: web::Data<Pool<SqliteConnectionManager>>) -> Result<HttpResponse, MyError> {
+    let conn = db.get()?;
+    let mut statement = conn.prepare("SELECT id, text FROM todo")?;
+    let rows = statement.query_map(params![], |row| {
+        let id = row.get(0)?;
+        let text = row.get(1)?;
+        Ok(TodoEntry {id, text})
+    })?;
+
     let mut entries = Vec::new();
+    for row in rows {
+        entries.push(row?)
+    }
     entries.push(TodoEntry {
         id: 1,
         text: "First entry".to_string(),
@@ -40,6 +60,20 @@ async fn index() -> Result<HttpResponse, MyError> {
 
 #[actix_rt::main]
 async fn main() -> Result<(), actix_web::Error> {
+    let manager = SqliteConnectionManager::file("todo.db");
+    let pool = Pool::new(manager).expect("Failed to init the connection pool");
+    let conn = pool
+        .get()
+        .expect("Failedt to get the connection from the pool");
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS todo (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            text TEXT NOT NULL
+        )",
+        params![],
+    )
+        .expect("Failed to create a todo table");
+
     HttpServer::new(move || App::new().service(index))
         .bind("0.0.0.0:8080")?
         .run()
